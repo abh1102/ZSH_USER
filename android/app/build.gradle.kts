fdc61f1d330c3cde1c7236687b98a716a2e4fb2f
abh1102 <abhishekjha2507@gmail.com>
2025-11-07 15:41:39 +0530

    build fixes

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 android {
     namespace = "com.zanaduhealth.beta"
     compileSdkVersion(35)
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
     compileOptions {
         sourceCompatibility(JavaVersion.VERSION_17)
         targetCompatibility(JavaVersion.VERSION_17)
